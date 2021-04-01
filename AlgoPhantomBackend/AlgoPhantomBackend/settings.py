ce60143102f5dba6fe02295771db5ba88801e704
Arif <arif.asan.jmi@gmail.com>
2021-04-01 21:18:47 +0530

    try except for local_setting

--- AlgoPhantomBackend/AlgoPhantomBackend/settings.py
+++ AlgoPhantomBackend/AlgoPhantomBackend/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 # from .local_settings import *
-
+try:
+  from local_settings import *
+except ImportError:
+  pass
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
